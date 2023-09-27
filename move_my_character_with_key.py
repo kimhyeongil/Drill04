fd308510ab1326ecc369dd36acfc1fbe49828af3
KHI(home) <alfkcjstk853@naver.com>
2023-09-28 00:50:49 +0900

    make punch animation complete

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -9,39 +9,48 @@ class character:
     def __init__(self):
         self.x, self.y = TUK_WIDTH // 2, TUK_HEIGHT // 2
         self.img = sprite
-        self.sizeW, self.sizeH = 100, 100
+        self.sizeW, self.sizeH = 2, 2
         self.frame = 0
         self.dirH, self.dirV = 0, 0
         self.speed = 10
-        self.width, self.height = 36, 36
-        self.left, self.bottom = 0, self.img.h - 36
-        self.nFrame = 6
-    
+        
+        self.lefts = list(range(0, 36 * 14 + 1, 36))
+        self.bottoms = [sprite.h - 41 for i in range(len(self.lefts))]
+        self.widths = [36 for i in range(len(self.lefts))]
+        self.heights = [41 for i in range(len(self.lefts))]
+        
+        self.lefts += [108, 144, 180, 224, 272, 320, 360, 396, 432, 468, 504, 545, 596, 647, 688]
+        self.widths += [36, 36, 44, 48, 48, 40, 36, 36, 36, 36, 41, 51, 51, 41, 36]
+        self.bottoms += [sprite.h - 82 for i in range(15)]
+        self.heights += [41 for i in range(15)]
+        
+        self.index = 0
     def draw(self):
-        self.img.clip_draw(self.left + self.frame * self.width,self.bottom, 36, 36, self.x, self.y, self.sizeW, self.sizeH)
+        self.img.clip_draw(self.lefts[self.frame + self.index], self.bottoms[self.frame + self.index], 
+                           self.widths[self.frame + self.index], self.heights[self.frame + self.index],
+                           self.x - (self.widths[self.frame + self.index] - 36) // 2, self.y,
+                           self.widths[self.frame + self.index] * self.sizeW, self.heights[self.frame + self.index] * self.sizeH)
         if (self.x + self.dirH * self.speed <= TUK_WIDTH - self.sizeW // 2 and self.x + self.dirH * self.speed >= self.sizeW // 2):
             self.x += self.dirH * self.speed
         if (self.y + self.dirV * self.speed <= TUK_HEIGHT - self.sizeH // 2 and self.y + self.dirV * self.speed >= self.sizeH // 2):
             self.y += self.dirV * self.speed
-        self.frame = (self.frame + 1) % self.nFrame
-
+        self.frame = ((self.frame + 1) % self.nFrame)
+    
     def set_idle(self):
-        self.width, self.height = 36, 36
-        self.left, self.bottom = 0, self.img.h - 36
-        self.nFrame = 6
+        self.nFrame, self.index = 15, 0
     
     def set_punch(self):
-        self.width, self.height = 36, 36
-        self.left, self.bottom = 108, self.img.h - 36
-        self.nFrame = 12        
+        self.nFrame, self.index = 15, 15
+        
+
 my_character = character()
 
-def animation_idle():
+def animation():
     clear_canvas()
     backGround.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     my_character.draw()
     update_canvas()
-    delay(0.1)
+    delay(0.15)
 
 def handle_events():
     global my_character
@@ -68,10 +77,14 @@ def handle_events():
     if (my_character.dirH == 0 and my_character.dirV == 0):
         my_character.set_idle()
 
-while True:
-    handle_events()
-    animation_idle()
 
+# while True:
+#     handle_events()
+#     animation()
+
+my_character.set_punch()
+while True:
+    animation()
 delay(1)
 
 close_canvas()
