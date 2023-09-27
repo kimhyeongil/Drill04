5aa2bf50b93c1989c35e4fa1dc167a20f217ec26
KHI(home) <alfkcjstk853@naver.com>
2023-09-27 20:03:02 +0900

    TUK_GROUND double buf complete

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,8 +4,9 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 sprite = load_image('sprite_sheet.png')
 backGround = load_image('TUK_GROUND.png')
-
-backGround.draw_now(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+clear_canvas()
+backGround.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+update_canvas()
 delay(1)
 
 close_canvas()
