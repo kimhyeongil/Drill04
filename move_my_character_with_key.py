12663d44736d8fd53bec230775d399bb78b82bb4
KHI(home) <alfkcjstk853@naver.com>
2023-09-27 21:21:43 +0900

    limit vertical range

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -16,7 +16,8 @@ class character:
         self.img.clip_draw(self.frame * 36,self.img.h - 36, 36, 36, self.x, self.y, self.w, self.h)
         if (self.x + self.dirH * 5 <= TUK_WIDTH - self.w // 2 and self.x + self.dirH * 5 >= self.w // 2):
             self.x += self.dirH * 5
-        self.y += self.dirV * 5
+        if (self.y + self.dirV * 5 <= TUK_HEIGHT - self.h // 2 and self.y + self.dirV * 5 >= self.h // 2):
+            self.y += self.dirV * 5
         self.frame = (self.frame + 1) % 6
 my_character = character()
 
