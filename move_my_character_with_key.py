a7448d52dbf346ac3ed38e849a912b6b96326e10
KHI(home) <alfkcjstk853@naver.com>
2023-09-28 01:04:34 +0900

    flip to look direction complete

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -14,19 +14,27 @@ class character:
         self.dirH, self.dirV = 0, 0
         self.speed = 10
         
-        self.lefts = list(range(0, 36 * 14 + 1, 36))
-        self.bottoms = [sprite.h - 41 for i in range(len(self.lefts))]
+        self.lefts = list(range(0, 36 * 13 + 1, 36))
+        self.bottoms = [sprite.h - 38 for i in range(len(self.lefts))]
         self.widths = [36 for i in range(len(self.lefts))]
-        self.heights = [41 for i in range(len(self.lefts))]
+        self.heights = [38 for i in range(len(self.lefts))]
         
         self.lefts += [108, 144, 180, 224, 272, 320, 360, 396, 432, 468, 504, 545, 596, 647, 688]
         self.widths += [36, 36, 44, 48, 48, 40, 36, 36, 36, 36, 41, 51, 51, 41, 36]
         self.bottoms += [sprite.h - 82 for i in range(15)]
-        self.heights += [41 for i in range(15)]
+        self.heights += [38 for i in range(15)]
         
         self.index = 0
+        self.isLookLeft = True
     def draw(self):
-        self.img.clip_draw(self.lefts[self.frame + self.index], self.bottoms[self.frame + self.index], 
+        if (not self.isLookLeft):
+            self.img.clip_composite_draw(self.lefts[self.frame + self.index], self.bottoms[self.frame + self.index], 
+                           self.widths[self.frame + self.index], self.heights[self.frame + self.index],
+                           0, 'h',
+                           self.x - (self.widths[self.frame + self.index] - 36) // 2, self.y,
+                           self.widths[self.frame + self.index] * self.sizeW, self.heights[self.frame + self.index] * self.sizeH)
+        else:
+            self.img.clip_draw(self.lefts[self.frame + self.index], self.bottoms[self.frame + self.index], 
                            self.widths[self.frame + self.index], self.heights[self.frame + self.index],
                            self.x - (self.widths[self.frame + self.index] - 36) // 2, self.y,
                            self.widths[self.frame + self.index] * self.sizeW, self.heights[self.frame + self.index] * self.sizeH)
@@ -37,10 +45,10 @@ class character:
         self.frame = ((self.frame + 1) % self.nFrame)
     
     def set_idle(self):
-        self.nFrame, self.index = 15, 0
+        self.nFrame, self.index = 14, 0
     
     def set_punch(self):
-        self.nFrame, self.index = 15, 15
+        self.nFrame, self.index = 15, 14
         
 
 my_character = character()
@@ -78,11 +86,14 @@ def handle_events():
         my_character.set_idle()
     else:
         my_character.set_punch()
-
+    if (my_character.dirH > 0):
+        my_character.isLookLeft = False
+    if (my_character.dirH < 0):
+        my_character.isLookLeft = True
 while True:
     handle_events()
     animation()
-    
+
 delay(1)
 
 close_canvas()
