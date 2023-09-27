f76143987339686e1c156f2258600a2ea895a8a7
KHI(home) <alfkcjstk853@naver.com>
2023-09-27 19:55:22 +0900

    load TUK_GROUNG complete

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -2,8 +2,9 @@ from pico2d import *
 open_canvas()
 
 sprite = load_image('sprite_sheet.png')
-sprite.draw_now(400,400)
+backGround = load_image('TUK_GROUND.png')
 
+backGround.draw_now(400,400)
 delay(1)
 
 close_canvas()
