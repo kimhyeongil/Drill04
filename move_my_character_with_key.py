23e20a0be9efcfb52770a5cbaf16adc6954cea8d
KHI(home) <alfkcjstk853@naver.com>
2023-09-27 20:32:18 +0900

    define characater.draw sprite sheet at center complete

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,9 +6,11 @@ sprite = load_image('sprite_sheet.png')
 backGround = load_image('TUK_GROUND.png')
 
 class character:
+    def __init__(self):
+        self.x, self.y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+        self.img = sprite
     def draw(self):
-        print('draw')
-
+        self.img.draw(self.x, self.y)
 my_character = character()
 
 clear_canvas()
