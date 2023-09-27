cfff1db0a1387ec0a1564f0d107ef769c9586e64
KHI(home) <alfkcjstk853@naver.com>
2023-09-27 20:01:53 +0900

    match size TUK_GROUND and canvas complete

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,10 +1,11 @@
 from pico2d import *
-open_canvas()
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 sprite = load_image('sprite_sheet.png')
 backGround = load_image('TUK_GROUND.png')
 
-backGround.draw_now(400,400)
+backGround.draw_now(TUK_WIDTH // 2, TUK_HEIGHT // 2)
 delay(1)
 
 close_canvas()
