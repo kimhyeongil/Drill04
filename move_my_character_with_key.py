9f6e63c1ac2a83ea8544360e2b5efaabb6830b69
KHI(home) <alfkcjstk853@naver.com>
2023-09-27 20:37:15 +0900

    scale frame complete

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -9,8 +9,9 @@ class character:
     def __init__(self):
         self.x, self.y = TUK_WIDTH // 2, TUK_HEIGHT // 2
         self.img = sprite
+        self.w, self.h = 100, 100
     def draw(self):
-        self.img.clip_draw(0,self.img.h - 36, 36, 36, self.x, self.y)
+        self.img.clip_draw(0,self.img.h - 36, 36, 36, self.x, self.y, self.w, self.h)
 my_character = character()
 
 clear_canvas()
