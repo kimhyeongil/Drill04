970306cd76feafaafd212ad50f1079008fb2d6dd
KHI(home) <alfkcjstk853@naver.com>
2023-09-27 20:25:42 +0900

    construct class character complete

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,6 +4,13 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 sprite = load_image('sprite_sheet.png')
 backGround = load_image('TUK_GROUND.png')
+
+class character:
+    pass
+
+my_character = character()
+print(type(my_character))
+
 clear_canvas()
 backGround.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
 update_canvas()
